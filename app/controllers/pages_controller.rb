a318ca08a1d9385eed78488ea1262ecc0ab2a413
varshnimurali <vmurali0@chicagobooth.edu>
2025-04-28 03:17:12 +0000

    save updates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,4 +17,38 @@ class PagesController < ApplicationController
   def square_root
     render({ :template => "templates/square_root" })
   end
+
+  def square_root_results
+    @the_num_2 = params.fetch("users_number_2").to_f
+    @the_result_2 = @the_num_2 ** 0.5
+  
+    render({ :template => "templates/square_root_results" })
+  end
+
+  def payment
+    render({ :template => "templates/payment" })
+  end
+
+  def payment_results
+    @APR = params.fetch("users_APR").to_f / 100 / 12
+    @Yrs = params.fetch("users_years").to_f * 12
+    @Principal = params.fetch("users_principal").to_f
+    @Numerator = @APR * @Principal
+    @Denominator = 1 - (1+@APR)**(-@Yrs)
+    @PMT = @Numerator / @Denominator
+
+    render({ :template => "templates/payment_results" })
+  end
+
+  def random
+    render({ :template => "templates/random" })
+  end
+
+  def random_results
+    @Minimum = params.fetch("min").to_f
+    @Maximum = params.fetch("max").to_f
+    @Random_Num = rand(@Minimum..@Maximum)
+  
+    render({ :template => "templates/random_results" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,14 @@ Rails.application.routes.draw do
   get("/square/results", { :controller => "pages", :action => "square_results"})
 
   get("/square_root/new", { :controller => "pages", :action => "square_root"})
+  
+  get("/square_root/results", { :controller => "pages", :action => "square_root_results"})
+
+  get("/payment/new", { :controller => "pages", :action => "payment"})
+
+  get("/payment/results", { :controller => "pages", :action => "payment_results"})
+
+  get("/random/new", { :controller => "pages", :action => "random"})
+
+  get("/random/results", { :controller => "pages", :action => "random_results"})
 end
